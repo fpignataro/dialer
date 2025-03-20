4dda7d14521b234290540a1e5f499f707646a9f3
fpignataro <fabian.pignataro84@gmail.com>
2025-03-20 12:11:35 -0300

    nat & oml aio

--- asterisk/build/docker-entrypoint.sh
+++ asterisk/build/docker-entrypoint.sh
@@ -14,23 +14,20 @@ if [[ -n "${ASTERISK_USER}" && -n "${ASTERISK_PASS}" ]]; then
     sed -i "s/aripassword/${ASTERISK_PASS}/g" /etc/asterisk/ari.conf
 fi
 
-# Configuración de HTTP
-if [[ -n "${ASTERISK_DIALER_HOSTNAME}" ]]; then
-    sed -i "s/0.0.0.0/${ASTERISK_DIALER_HOSTNAME}/g" /etc/asterisk/http.conf
-fi
-
 # Configuración de OMLeads ACD PJSIP peer
-if [[ -n "${OMLACD_SIP_ADDR}" ]]; then
-    sed -i "s/acd:5260/${OMLACD_SIP_ADDR}/g" /etc/asterisk/pjsip_wizard_omlacd.conf
+if [[ -n "${OMLACD_SIP_SERVER}" ]]; then
+    sed -i "s/acd/${OMLACD_SIP_SERVER}/g" /etc/asterisk/pjsip_wizard_omlacd.conf
 fi
 
 # Configuración de NAT & SIP
-if [[ -n "${ASTERISK_SIP_NAT_ADDR}" ]]; then
-    sed -i "s#;external_media_address=localhost#external_media_address=${ASTERISK_SIP_NAT_ADDR}#g" /etc/asterisk/pjsip.conf
-    sed -i "s#;external_signaling_address=localhost#external_signaling_address=${ASTERISK_SIP_NAT_ADDR}#g" /etc/asterisk/pjsip.conf    
-fi
+if [[ -n "${SIP_NAT_ADDR}" ]]; then
+    sed -i "s#;external_media_address=localhost#external_media_address=${SIP_NAT_ADDR}#g" /etc/asterisk/pjsip.conf
+    sed -i "s#;external_signaling_address=localhost#external_signaling_address=${SIP_NAT_ADDR}#g" /etc/asterisk/pjsip.conf    
+else
+    echo "NAT mode selected was not found !!!"
+fi  
 
-# Configuración opcional para SIP Gateway PSTN
+# Configuración opcional para SIP Gateway PSTN 
 if [[ -n "${PSTNGW_REGISTER}" && "${PSTNGW_REGISTER}" == "yes" ]]; then
     sed -i "s/sends_registrations=no/sends_registrations=yes/g" /etc/asterisk/pjsip_wizard_pstngw.conf
 fi
@@ -47,8 +44,9 @@ fi
 if [[ -n "${PSTNGW_HOSTNAME}" ]]; then
     sed -i "s/tel_gateway:5260/${PSTNGW_HOSTNAME}/g" /etc/asterisk/pjsip_wizard_pstngw.conf
 else
-    sed -i "s/tel_gateway:5260/${OMLACD_SIP_ADDR}/g" /etc/asterisk/pjsip_wizard_pstngw.conf
+    sed -i "s/tel_gateway/${OMLACD_SIP_SERVER}/g" /etc/asterisk/pjsip_wizard_pstngw.conf
 fi
+
 
 # Iniciar el servidor Asterisk
 echo "**[omlacd] Initializing Asterisk"
