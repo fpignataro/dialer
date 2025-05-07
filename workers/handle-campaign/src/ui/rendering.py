8e6f401aee84189e10908ef8c3adec4edeaf0c1a
Alejandro Zamora <Alejandro Zamora Fonseca>
2025-05-07 17:07:57 -0300

    - refactored templates - completed initial rendering

--- workers/handle-campaign/src/ui/rendering.py
+++ workers/handle-campaign/src/ui/rendering.py
@@ -22,7 +22,7 @@ ENVIRONMENT.globals['STATUS_TO_ACTION'] = STATUS_TO_ACTION
 class AdminRender():
     @classmethod
     def render_init(cls, campaigns):
-        return INIT_TEMPLATE.render(campaigns=campaigns)
+        return INIT_TEMPLATE.render(campaigns=campaigns, running=True)
 
     @classmethod
     def render_status_change(cls, id_campaign, new_status, new_status_str, actions_campaign):
