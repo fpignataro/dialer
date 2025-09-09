5cbd20f328b2b83aee1bf76b917dd2d121329309
Alejandro Zamora <ale2014.zamora@gmail.com>
2025-09-08 21:33:30 -0300

    attempt

--- run-tests.sh
+++ run-tests.sh
@@ -6,6 +6,6 @@ else
     docker-compose --env-file .env-tests -f docker-compose-test.yml up -d --remove-orphans
 fi
 
-docker exec omnidialer-worker-test python -m unittest tests.py
+docker exec omnidialer-worker-test python -m unittest tests.py --failfast
 
 docker-compose --env-file .env-tests -f docker-compose-test.yml down
