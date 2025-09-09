9a36c3c42df2569e8ce84b182d4cc45692175118
Alejandro Zamora <ale2014.zamora@gmail.com>
2025-09-08 21:15:19 -0300

    - fixes

---
+++ run-tests.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+if [ -z "$1" ]; then
+    docker-compose --env-file .env-tests -f docker-compose-test.yml up -d --build --remove-orphans
+else
+    docker-compose --env-file .env-tests -f docker-compose-test.yml up -d --remove-orphans
+fi
+
+docker exec -it omnidialer-worker-test python -m unittest tests.py
+
+docker-compose --env-file .env-tests -f docker-compose-test.yml down
